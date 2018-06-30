64c1f98a5cbf52f8a051ff445d95707586465bc9
Joosung Park <park012241@park012241.me>
2018-06-30 23:17:29 +0900

    급식 받아오기 틀

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return 'I am DMS Assistant!'
 
+@app.route('/meal')
+def getmeal():
+    return 'meal'
 
 if __name__ == '__main__':
     app.run()
