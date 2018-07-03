cba4c37f31fd41a4c405dbd4cc4d9d5ff049bb0a
Joosung Park <park012241@park012241.me>
2018-07-03 20:00:12 +0900

    리스트??

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DMS-Assistant
-DMS on Google Assistant
+<p>DMS on Google Assistant<br>
+DMS의 서비스를 구글 어시스턴트에서</p>
+
+<p>현재 급식정보만 지원합니다.<br>
+다른 기능은 추후에 구현...헤헤</p>
+
+<p>Actions on Google 과 DialogFlow를 사용했고,<br>
+Azure 위에서 Fulfillment를 설정하였습니다.</p>
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def meal(param):
     rawMeal = ''
     jsonMeal = ''
     meal = ''
-    listMeal=''
+    listMeal = []
 
     if (param["day"] == "오늘"):
         rawMeal = json.loads(
@@ -37,15 +37,50 @@ def meal(param):
     for i in jsonMeal:
         meal += i + ', '
     for i in jsonMeal:
-        listMeal
+        listMeal.append({
+            "optionInfo": {
+                "key": "meal",
+                "synonyms": []
+            },
+            "title": i
+        })
 
     meal = meal[0:-2]  # 여기까지 급식 받아오기
 
     mealTxt = param["day"] + "의 " + param["meal"] + "은 " + meal + "입니다."
-    req = {"fulfillmentText": mealTxt, "payload": {"google": {"expectUserResponse": False, "richResponse": {
+    textreq = {"fulfillmentText": mealTxt, "payload": {"google": {"expectUserResponse": False, "richResponse": {
         "items": [{"simpleResponse": {"textToSpeech": mealTxt}}]}}}}
 
-    return req
+    listreq = {
+        "conversationToken": "",
+        "expectUserResponse": False,
+        "expectedInputs": [
+            {
+                "inputPrompt": {
+                    "initialPrompts": [
+                        {
+                            "textToSpeech": mealTxt
+                        }
+                    ],
+                    "noInputPrompts": []
+                },
+                "possibleIntents": [
+                    {
+                        "intent": "actions.intent.OPTION",
+                        "inputValueData": {
+                            "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                            "listSelect": {
+                                "title": param["day"] + "의 " + param["meal"],
+                                "items": listMeal
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+    return listreq
 
 
 @app.route('/')
